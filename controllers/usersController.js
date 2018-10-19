568dea1681ea45b687e09245fa990423e9378c2c
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-10-19 12:06:04 -0600

    Added routes for users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -23,16 +23,63 @@ router.get("/", async (req, res, next) => {
     }
 });
 // NEW ROUTE
+router.get('/new', (req, res)=>{
+    res.render('users/new.ejs');
+});
 
 // SHOW ROUTE
+router.get('/:id', async (req, res, next) => {
+    try {
+        const foundUser = await User.findById(req.params.id);
+        res.render("users/show.ejs", {
+            user: foundUser
+        });
+    } catch (err) {
+        next(err);
+    }
+});
 
 // EDIT ROUTE
+router.get('/:id/edit', async (req, res, next) => {
+    try {
+        const foundUser = await User.findById(req.params.id);
+        res.render('users/edit.ejs', {
+            user: foundUser
+        });
+    } catch (err) {
+        next(err);
+    }
+});
 
 // CREATE ROUTE
+router.post('/', async (req, res, next) => {
+    try {
+        const newUser = await User.create(req.body);
+        res.redirect('/users');
+    } catch (err) {
+        next(err);
+    }
+});
 
 // UPDATE ROUTE
+router.put('/:id', async (req, res, next) => {
+    try {
+        const newUser = await User.findByIdAndUpdate(req.params.id, req.body);
+        res.redirect(`/users/${req.params.id}`);
+    } catch (err) {
+        next(err);
+    }
+});
 
 // DELETE ROUTE
+router.delete('/:id', async (req, res) => {
+    try {
+        const deltedUser = await User.findByIdAndDelete(req.params.id);
+        res.redirect('/users');
+    } catch (err) {
+        next (err);
+    }
+});
 
 // EXPORT ROUTER
 
