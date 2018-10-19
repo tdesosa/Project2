f47ccfd26dde0fe945258ed626fc661af50a9edc
tdesosa <tdesosa@gmail.com>
2018-10-19 12:19:06 -0600

    barberController routes setup

--- controllers/barbersController.js
+++ controllers/barbersController.js
@@ -27,15 +27,74 @@ router.get('/', async (req, res, next) => {
 
 // NEW ROUTE
 
+router.get('/new', (req, res, next) => {
+    res.render('barbers/new.ejs', {
+    });
+});
+
 // SHOW ROUTE
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const foundBarber = await Barber.findById(req.params.id);
+
+        res.render('barbers/show.ejs', {
+            barber: foundBarber
+        });
+    } catch (err) {
+        next(err);
+    }
+});
 
 // EDIT ROUTE
 
+router.get('/:id/edit', async (req, res, nex) => {
+    try {
+        const foundBarber = await Barber.findById(req.params.id);
+
+        res.render('barbers/edit.ejs', {
+            barber: foundBarber
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
 // CREATE ROUTE
+
+router.post('/', async (req, res, next) => {
+    try {
+        const newBarber = await Barber.crete(req.body);
+
+        res.redirect('/barbers');
+    } catch (err) {
+        next(err);
+    }
+});
 
 // UPDATE ROUTE
 
+router.put('/:id', async (req, res, next) => {
+    try {
+        const newBarber = await Barber.findByIdAndUpdate(req.params.id, req.body);
+
+        res.redirect(`/barbers/${req.params.id}`);
+    } catch (err) {
+        next(err);
+    }
+});
+
 // DELETE ROUTE
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const deletedBarber = await Barber.findByIdAndDelete(req.params.id);
+
+        res.redirect('/barbers');
+    } catch (err) {
+        next(err);
+    }
+});
 
 // EXPORT ROUTER
 
