f2310dd2f1191ab8f3fe405bd2297cc9df6cbf69
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-10-21 13:24:24 -0600

    working on encryption

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,16 +2,65 @@
 
 const express = require('express');
 const bcrypt  = require('bcryptjs');
-
 // REQUIRE MODELS
 
-// cost User = require('../models/userModel');
+const User = require('../models/userModel');
 
 // EXPRESS ROUTER METHOD
 
 const router  = express.Router();
 
+
+router.get('/', (req, res) => {
+    req.session.username = 'username'
+    req.session.password = 'password'
+});
+
+
 // LOGIN ROUTE
+router.post('/login', (req, res) => {
+    req.session.username = req.body.username;
+    req.session.password = req.body.password;
+    req.session.logged   = true;
+    console.log(req.session);
+    res.redirect('/authors')
+  });
+
+
+
+// router.post('/login', async(req, res, next) => {
+//     try {
+//         console.log(req.body)
+//         const user = await User.find({username: req.body.username});
+//         const validLogin = await bcrypt.compare(req.body.password, user.password);
+//         console.log(validLogin);
+//         req.session.userId = user._id;
+//         res.redirect('/login');
+//     } catch(err) {
+//         next(err)
+//     }
+// });
+
+// router.post('/', async(req, res) => {
+//     try{
+//         if(!req.session.userId){
+//             res.render('/auth/login', {
+//                 message: "you must be logged in to do that"
+//             })
+//         } else{
+//             const newReview = {
+//                 title: req.body.title,
+//                 body: req.body.body,
+//                 // etc etc from the schema
+//                 rating: req.body.rating,
+//                 coffee: req.body.cofee,
+//                 reviewer: req.session.userId
+//             }
+//         }
+//     } catch(err){
+//         next(err)
+//     }
+// });
 
 // REGISTER ROUTE
 
@@ -21,4 +70,4 @@ const router  = express.Router();
 
 // EXPORT ROUTER
 
-module.export = router;
+module.exports = router;
--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express        = require('express');
 const app            = express();
 const bodyParser     = require('body-parser');
 const methodOverride = require('method-override');
-const session        = require('express-sesssion');
-// const bcrypt         = require('bcryptjs');
+const session        = require('express-session');
+const bcrypt         = require('bcryptjs');
 
 // REQUIRE ACCESS TO MONGO VIA MONGOOSSE
 
@@ -13,30 +13,34 @@ require('./db/db');
 
 // REQUIRE CONTROLLERS
 
-// const authController  = require('./controllers/authController');
+const authController  = require('./controllers/authController');
 const usersController  = require('./controllers/usersController');
 const barbersController = require('./controllers/barbersController');
 
 // MIDDLEWARE SETUP
+app.use(session({
+    secret: "Secret String",
+    resave: false,
+    saveUninitialized: false
+   
+}));
 
-// app.use(session({
-//     secret: 'Secret String',
-//     resave: false,
-//     saveUninitialized: false
-// }));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
-// app.use('/auth', authController);
+app.use('/auth', authController);
 app.use('/users', usersController);
 app.use('/barbers', barbersController);
 
 // LANDING PAGE
-
 app.get('/', (req, res) => {
-    res.render('landing.ejs');
+    res.render("login.ejs")
 });
+
+// app.get('/', (req, res) => {
+//     res.render('landing.ejs');
+// });
 
 // SET PORT VARIABLE
 
